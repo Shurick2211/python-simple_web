c92ae171251b3448de81bbca396e253e2d20dfe1
AlexNimko <alex.nimko@playchi.games>
2025-02-03 09:36:28 +0200

    add params for get

--- main.py
+++ main.py
@@ -8,8 +8,9 @@ web = server.Server()
 def hello():
     return "{\"message\":\"Hello, World!\"}"
 
-@web.get("/about", app_type="application/json")
-def about():
+@web.get("/about", app_type="application/json", paramm=["login","pass"])
+def about(params):
+    print(f"Params - {params}")
     return "{\"message\":\"This is a simple Python web server\"}"
 
 @web.get("/")
--- server.py
+++ server.py
@@ -10,16 +10,20 @@ class Server:
     self.__routes = {}
     self.__host = host
     self.__port = port
+    self.__dict_param = {}
 
   def async_start_server(self):
     _thread.start_new_thread(self.start_server, ())
 
-  def get(self, path, app_type="text/html"):
+  def get(self, path, app_type="text/html", paramm=None):
       """Decorator to register a function for a specific GET path."""
       def decorator(func):
           @wraps(func)
           def wrapper():
-              return func()
+            if paramm and len(self.__dict_param)>0:
+              in_params =list(map(lambda i: self.__dict_param.get(i), paramm))
+              return func(in_params)
+            else: return func()
           self.__routes[path] = wrapper
           self.__routes[path + 'app_type'] = app_type
           return wrapper
@@ -37,6 +41,14 @@ class Server:
 
       if method != "GET":
           return "HTTP/1.1 405 Method Not Allowed\nContent-Type: text/html\n\n<h1>405 Method Not Allowed</h1>"
+
+      path_params = path.split("?")
+      path = path_params[0]
+
+      if len(path_params) > 1:
+        params=path_params[1].split("&")
+        params = list(map(lambda i: i.split("="), params))
+        self.__dict_param = dict(params)
 
       # Check if the path exists in routes
       if path in self.__routes:
